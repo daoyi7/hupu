6903260dbc73aadb24f5a51ed01ea526eb9026ed
daoyi7 <kawhichina@gmail.com>
2017-11-22 21:46:10 +0800

    update voice api

--- server/router/voice/voice.js
+++ server/router/voice/voice.js
@@ -17,53 +17,43 @@ app.get('/', (req, res, next) => {
       const $ = cheerio.load(sres.text)
       let urls = []
 
-      $(".news-list li").each((idx, ele) => {
+      $(".news-list li").slice(0, 20).each((idx, ele) => {
         const $ele = $(ele)
 
         if(typeof($ele.attr("class"))=="undefined") {
           const href = $ele.find("h4 a").attr("href")
 
           urls.push(href)
-          // api.push({
-          //   id: href.split("").slice(27,34).join(""),
-          //   title: $ele.find("h4 a").text().trim(),
-          //   href: href,
-          //   upTime: $ele.find(".other-left a").eq(0).text().trim(),
-          //   via: $ele.find(".comeFrom a").text().trim(),
-          // })
         }
       })
 
-      // res.send(api)
-
       let ep = new eventproxy()
 
-      // ep.after("voice_html", api.length, (voice) => {
-      //   voice = voice.map((voice_item) => {
-      //     let id = voice_item[0]
-      //   })
-      // })
+      urls.forEach((voice_url) => {
+        superagent.get(voice_url)
+          .end((err, sres) => {
+            ep.emit('voice_html', [voice_url, sres.text])
+          })
+      })
 
-      ep.after('voice_html', urls.length, (voice) => {
-        voice = voice.map((voice_item) => {
-          let $ = cheerio.load(voice_item)
+      ep.after('voice_html', urls.length, (url) => {
+        url = url.map((url_item) => {
+          let href = url_item[0]
+          let html = url_item[1]
+          let $ = cheerio.load(html)
+
           return ({
-            href: voice_item,
+          href: href,
+            id: href.split("").slice(27,34).join(""),
             title: $('.headline').text().trim(),
-            // first_comment: $('#readfloor td').eq(0).text().trim()
+            upTime: $(".artical-info a.time span").eq(0).text().trim(),
+            source: $(".comeFrom a").eq(0).text().trim(),
+            thumb: $('.artical-importantPic img').eq(0).attr("src"),
+            info: $('.artical-main-content p').eq(0).text().trim()
           })
         })
 
-        console.log(11)
-        res.send(voice)
-      })
-      //
-      urls.slice(0, 5).forEach((voice_url) => {
-        superagent.get(voice_url)
-          .end((err, sres) => {
-            console.log('fetch' + voice_url + 'successful')
-            ep.emit('topic_html', [voice_url, sres.text])
-          })
+        res.send(url)
       })
     })
 })
