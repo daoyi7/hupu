a1133f70996a440c2ae19fad341e3305bebdce81
daoyi7 <kawhichina@gmail.com>
2017-11-23 14:55:44 +0800

    update voice api

--- server/router/voice.js
+++ server/router/voice.js
@@ -80,19 +80,25 @@ app.get('/', (req, res, next) => {
       const $ = cheerio.load(sres.text)
       let urls = []
 
-      $(".news-list li").each((idx, ele) => {
+      $(".news-list li").slice(0, 10).each((idx, ele) => {
         const $ele = $(ele)
 
         if (typeof($ele.attr("class")) == "undefined") {
-          const href = $ele.find("h4 a").attr("href")
+          const voice_href = $ele.find("h4 a").attr("href")
+          const bbs_href = $ele.find(".other-right a").attr("href")
 
-          urls.push(href)
+          urls.push({
+            id: idx,
+            voice_url: voice_href,
+            bbs_url: bbs_href,
+            bbs_url_id: bbs_href.split("").slice(21, 29).join("")
+          })
         }
       })
 
       let concurrencyCount = 0
 
-      const fetchUrl = (url, callback) => {
+      const fetchUrl = (url, callback, index) => {
         let delay = parseInt((Math.random() * 10000000) % 2000, 10)
 
         concurrencyCount++
@@ -103,35 +109,35 @@ app.get('/', (req, res, next) => {
 
           superagent.get(url)
             .end((err, sres) => {
-              if(err) {
-                console.error("我要看这个："+err)
+              if (err) {
+                console.error("我要看这个：" + err)
               }
 
               let $ = cheerio.load(sres.text)
 
-              callback (null, {
+              callback(null, {
                 href: url,
-                id: url.split("").slice(27,34).join(""),
+                id: url.split("").slice(27, 34).join(""),
                 title: $('.headline').text().trim(),
                 upTime: $(".artical-info a.time span").eq(0).text().trim(),
                 source: $(".comeFrom a").eq(0).text().trim(),
                 thumb: $('.artical-importantPic img').eq(0).attr("src"),
-                info: $('.artical-main-content p').eq(0).text().trim()
+                info: $('.artical-main-content p').eq(0).text().trim(),
+                urls: urls[index >= 17 ? index - 1 : index]
               })
             })
 
-          // callback(null,url)
         }, delay)
       }
 
-      async.mapLimit(urls, 5, (url, callback) => {
-        fetchUrl(url, callback)
+      async.mapLimit(urls, 5, (url, callback, index) => {
+        fetchUrl(url.voice_url, callback, url.id)
       }, (err, result) => {
         console.log('final:')
         console.log(result)
         res.send(result)
       })
-  })
+    })
 })
 
 module.exports = app
